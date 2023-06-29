efc0ff1f2b11091bd65c16c3b1bf4a1a5694ad7d
Sebastian <sebastiansloma@gmail.com>
2023-06-29 12:24:02 +0200

    added img to page

--- src/Gallery.js
+++ src/Gallery.js
@@ -1,21 +1,37 @@
+/* eslint-disable jsx-a11y/alt-text */
 import React from 'react';
+import Img1 from './img/img1.jpg'
+import Img2 from './img/img2.jpg'
+import Img3 from './img/img3.jpg'
 
 const Gallery = () => {
 	let data = [
 		{
 			id: 1,
-			imgSrc: '',
+			imgSrc: Img1,
 		},
 		{
 			id: 2,
-			imgSrc: '',
+			imgSrc: Img2,
 		},
 		{
 			id: 3,
-			imgSrc: '',
+			imgSrc: Img3,
 		},
 	];
-	return <h1>Gallery</h1>;
+	return (
+        <>
+        <div className='gallery'>
+            {data.map((item, index)=>{
+                return(
+                    <div className='pics' key={index}>
+                        <img src={item.imgSrc} style={{width:'100%'}}/>
+                    </div>
+                )
+            })}
+        </div>
+        </>
+    )
 };
 
 export default Gallery;
