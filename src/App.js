aaf4dab275eda2904e4fb21741e26e04d0acf4a4
Sebastian <sebastiansloma@gmail.com>
2023-06-29 12:02:11 +0200

    added gallery.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React from 'react'
+import React from 'react';
 import './App.css';
+import Gallery from './Gallery';
 
 function App() {
-  return (
-    <>
-    <h1 style={{textAlign:'center'}}>Image Gallery</h1>
-    </>
-  );
+	return (
+		<>
+			<h1 style={{ textAlign: 'center' }}>Image Gallery</h1>
+			<Gallery />
+		</>
+	);
 }
 
 export default App;
