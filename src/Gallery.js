2747dba68d4ddf87f4526f75e72fc3e66e9d3886
Sebastian <sebastiansloma@gmail.com>
2023-07-01 13:23:12 +0200

    styled single pic

--- src/Gallery.js
+++ src/Gallery.js
@@ -1,9 +1,9 @@
 /* eslint-disable jsx-a11y/alt-text */
 import React, { useState } from 'react';
-import './gallery.css'
-import Img1 from './img/img1.jpg'
-import Img2 from './img/img2.jpg'
-import Img3 from './img/img3.jpg'
+import './gallery.css';
+import Img1 from './img/img1.jpg';
+import Img2 from './img/img2.jpg';
+import Img3 from './img/img3.jpg';
 
 const Gallery = () => {
 	let data = [
@@ -21,29 +21,34 @@ const Gallery = () => {
 		},
 	];
 
-    const [model, setModel] = useState(false)
-    const[tempimgSrc, setTempImgSrc] = useState('')
-    const getImg = (imgSrc) =>{
-        setTempImgSrc(imgSrc)
-        setModel(true)
-    }
+	const [model, setModel] = useState(false);
+	const [tempimgSrc, setTempImgSrc] = useState('');
+    
+	const getImg = (imgSrc) => {
+		setTempImgSrc(imgSrc);
+		setModel(true);
+	};
 
 	return (
-        <>
-        <div className={model ? "model open" : "model"}>
-            <img src={tempimgSrc}/>
-        </div>
-        <div className='gallery'>
-            {data.map((item, index)=>{
-                return(
-                    <div className='pics' key={index} onClick={() => getImg(item.imgSrc)}>
-                        <img src={item.imgSrc} style={{width:'100%'}}/>
-                    </div>
-                )
-            })}
-        </div>
-        </>
-    )
+		<>
+			<div className={model ? 'model open' : 'model'}>
+				<img src={tempimgSrc} />
+			</div>
+			<div className='gallery'>
+				{data.map((item, index) => {
+					return (
+						<div
+							className='pics'
+							key={index}
+							onClick={() => getImg(item.imgSrc)}
+						>
+							<img src={item.imgSrc} style={{ width: '100%' }} />
+						</div>
+					);
+				})}
+			</div>
+		</>
+	);
 };
 
 export default Gallery;
